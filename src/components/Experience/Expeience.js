c3fd8c7f5359dace0248b19dd22851cb3077edec
nguye3hg <88401567+nguye3hg@users.noreply.github.com>
2024-08-21 23:28:26 -0400

    update

--- src/components/Experience/Expeience.js
+++ src/components/Experience/Expeience.js
@@ -1,126 +1,121 @@
-import './Experience.css';
-import Cat1 from './Cat1.png';
-import Cat2 from './Cat2.png';
-import star1 from './star1.png';
-import moonandcat from './moonandcat.png';
-import UClogo from './UClogo.png';
-let appear=false;
-let turnLight=false;
-function yellow(){
-    document.getElementById('lampLight2').style.backgroundColor='yellow';
+import "./Experience.css";
+import Cat1 from "./Cat1.png";
+import Cat2 from "./Cat2.png";
+import star1 from "./star1.png";
+import moonandcat from "./moonandcat.png";
+import UClogo from "./UClogo.png";
+let appear = false;
+let turnLight = false;
+function yellow() {
+  document.getElementById("lampLight2").style.backgroundColor = "yellow";
 }
-function transparent(){
-    document.getElementById('lampLight2').style.backgroundColor='transparent';
+function transparent() {
+  document.getElementById("lampLight2").style.backgroundColor = "transparent";
 }
-function turnonLight1(){
-    if(turnLight){
-        turnLight=false;
-      document.getElementById('lampLight1').style.backgroundColor='transparent';
-      setTimeout(transparent,700);
-     
-  
-    }else{
-        turnLight=true;
-      document.getElementById('lampLight1').style.backgroundColor='yellow';
-      setTimeout(yellow,700);
-      
-      
-    }
+function turnonLight1() {
+  if (turnLight) {
+    turnLight = false;
+    document.getElementById("lampLight1").style.backgroundColor = "transparent";
+    setTimeout(transparent, 700);
+  } else {
+    turnLight = true;
+    document.getElementById("lampLight1").style.backgroundColor = "yellow";
+    setTimeout(yellow, 700);
   }
-function changeCat(){
-    if(appear){
-        document.getElementById('Cat1').style.opacity='1';
-        document.getElementById('Cat1').style.visibility='visible';
-        document.getElementById('Cat1').style.transition='opacity 2s linear';
+}
+function changeCat() {
+  if (appear) {
+    document.getElementById("Cat1").style.opacity = "1";
+    document.getElementById("Cat1").style.visibility = "visible";
+    document.getElementById("Cat1").style.transition = "opacity 2s linear";
 
-        appear=false;
-    }else{
-        document.getElementById('Cat1').style.opacity='0';
-        document.getElementById('Cat1').style.visibility='hidden';
-        document.getElementById('Cat1').style.transition='visibility 0s 2s, opacity 2s linear';
-        appear=true;
-    }
+    appear = false;
+  } else {
+    document.getElementById("Cat1").style.opacity = "0";
+    document.getElementById("Cat1").style.visibility = "hidden";
+    document.getElementById("Cat1").style.transition =
+      "visibility 0s 2s, opacity 2s linear";
+    appear = true;
+  }
 }
 
-function Experience(){
-    return (
-        <div id='expContainer'>
-            <div id='Experience'>
-                <div id='ExpContainer'>
-                    <div id='curvedLamp1'  onClick={turnonLight1}>
-                        <div id='hider1'></div>
-                        <div id='lampCone1'></div>
-                        <div id='lampBulb1'></div>
-                        <div id='lampLight1'></div>
-                        <div id='curvedLamp2'>
-                            <div id='hider2'></div>
-                            <div id='lampCone2'></div>
-                            <div id='lampBulb2'></div>
-                            <div id='lampLight2'></div>
-                        </div>
-                    </div>
-                    <div id='standingLamp'>
-                        
-                     </div>
-                    <div id='person'> 
-                        <div id='headPerson'></div>
-                        <div id='bodyPerson'></div>
-                        <div id='handPerson1'></div>
-                        <div id='handPerson2'></div>
-                        <div id='legPerson1'></div>
-                        <div id='legPerson2'></div>
-
-                    </div>
-                    <div id='cat'>
-                        <img onClick={changeCat} src={Cat1} id='Cat1' alt=''></img>
-                        <img src={moonandcat} id='moonandcat' alt=''></img>
-                    </div>
-                    <div id='slider'> 
-                        <h2>MY EXPERIENCE</h2><br></br>
-                        <h3>
-                            <div id='logoContainer1'>
-                                <img src={UClogo} alt=''></img>
-                            </div>
-                            <div id='TA'>
-                                Teaching assistant at UC
-                            </div>
-                        </h3><br></br>
-                        <h3>
-                            <div id='logoContainer2'>
-                                <img src={UClogo} alt=''></img>
-                            </div>
-                            <div id='Coop1'>
-                                 UC center for eutrepeneurship Coop
-                            </div>                        
-                        </h3><br></br>
-
-                    </div>
-                </div>
-                <div id='belowContainer'>
-                    <div id='floor'>
-                    
-
-                    </div>
-                    <div id='pillarContainer'>
-                        <div id='pillar1'></div>
-                        <div id='pillar2'></div>
-                        <div id='pillar3'></div>
-                        <div id='pillar4'></div>
-                        <div id='pillar5'></div>
-                        <div id='pillar6'></div>
-                        <div id='pillar7'></div>
-                        <div id='pillar8'></div>
-                    </div>
-                </div>
-                
-
+function Experience() {
+  return (
+    <div id="expContainer">
+      <div id="Experience">
+        <div id="ExpContainer">
+          <div id="curvedLamp1" onClick={turnonLight1}>
+            <div id="hider1"></div>
+            <div id="lampCone1"></div>
+            <div id="lampBulb1"></div>
+            <div id="lampLight1"></div>
+            <div id="curvedLamp2">
+              <div id="hider2"></div>
+              <div id="lampCone2"></div>
+              <div id="lampBulb2"></div>
+              <div id="lampLight2"></div>
             </div>
-            <div id='expTransition'>
-
-            </div>
+          </div>
+          <div id="standingLamp"></div>
+          <div id="person">
+            <div id="headPerson"></div>
+            <div id="bodyPerson"></div>
+            <div id="handPerson1"></div>
+            <div id="handPerson2"></div>
+            <div id="legPerson1"></div>
+            <div id="legPerson2"></div>
+          </div>
+          <div id="cat">
+            <img onClick={changeCat} src={Cat1} id="Cat1" alt=""></img>
+            <img src={moonandcat} id="moonandcat" alt=""></img>
+          </div>
+          <div id="slider">
+            <h2>MY EXPERIENCE</h2>
+            <br></br>
+            <h3>
+              <div id="logoContainer1">
+                <img src={UClogo} alt=""></img>
+              </div>
+              <div id="TA">
+                Software Engineering Co-op, IRAS Lab, University of Cincinnati
+              </div>
+            </h3>
+            <br></br>
+            <h3>
+              <div id="logoContainer1">
+                <img src={UClogo} alt=""></img>
+              </div>
+              <div id="TA">Front-End Developer, Pam Air, Ha Noi, Viet Nam</div>
+            </h3>
+            <br></br>
+            <h3>
+              <div id="logoContainer2">
+                <img src={UClogo} alt=""></img>
+              </div>
+              <div id="Coop1">
+                CS Intern, BCB-IRROPS, FPT Software, Ha Noi, Viet Nam
+              </div>
+            </h3>
+            <br></br>
+          </div>
         </div>
-        
-    );
+        <div id="belowContainer">
+          <div id="floor"></div>
+          <div id="pillarContainer">
+            <div id="pillar1"></div>
+            <div id="pillar2"></div>
+            <div id="pillar3"></div>
+            <div id="pillar4"></div>
+            <div id="pillar5"></div>
+            <div id="pillar6"></div>
+            <div id="pillar7"></div>
+            <div id="pillar8"></div>
+          </div>
+        </div>
+      </div>
+      <div id="expTransition"></div>
+    </div>
+  );
 }
-  
-  export default Experience;
+
+export default Experience;
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,197 +1,190 @@
-import './Work.css';
-import Contact from '../Contact/Contact';
-import pj1 from './pj1.png';
-import pj2 from './pj2.png';
-import pj3 from './pj3.png';
-import pj4 from './pj4.png';
-let light=false;
+import "./Work.css";
+import Contact from "../Contact/Contact";
+import pj1 from "./pj1.png";
+import pj2 from "./pj2.png";
+import pj3 from "./pj3.png";
+import pj4 from "./pj4.png";
+let light = false;
 
-function lightOnTransparent1(){
-  document.getElementById('light2').style.backgroundColor='transparent';
+function lightOnTransparent1() {
+  document.getElementById("light2").style.backgroundColor = "transparent";
 }
-function lightOnTransparent2(){
-  document.getElementById('light3').style.backgroundColor='transparent';
+function lightOnTransparent2() {
+  document.getElementById("light3").style.backgroundColor = "transparent";
 }
-function lightOnTransparent3(){
-  document.getElementById('light4').style.backgroundColor='transparent';
+function lightOnTransparent3() {
+  document.getElementById("light4").style.backgroundColor = "transparent";
 }
-function lightOnTransparent4(){
-  document.getElementById('light5').style.backgroundColor='transparent';
+function lightOnTransparent4() {
+  document.getElementById("light5").style.backgroundColor = "transparent";
 }
-function lightOnTransparent5(){
-  document.getElementById('light6').style.backgroundColor='transparent';
+function lightOnTransparent5() {
+  document.getElementById("light6").style.backgroundColor = "transparent";
 }
-function lightOnTransparent6(){
-  document.getElementById('light7').style.backgroundColor='transparent';
+function lightOnTransparent6() {
+  document.getElementById("light7").style.backgroundColor = "transparent";
 }
-function lightOnTransparent7(){
-  document.getElementById('light8').style.backgroundColor='transparent';
+function lightOnTransparent7() {
+  document.getElementById("light8").style.backgroundColor = "transparent";
 }
-function lightOnYellow1(){
-  document.getElementById('light2').style.backgroundColor='yellow';
+function lightOnYellow1() {
+  document.getElementById("light2").style.backgroundColor = "yellow";
 }
-function lightOnYellow2(){
-  document.getElementById('light3').style.backgroundColor='yellow';
+function lightOnYellow2() {
+  document.getElementById("light3").style.backgroundColor = "yellow";
 }
-function lightOnYellow3(){
-  document.getElementById('light4').style.backgroundColor='yellow';
+function lightOnYellow3() {
+  document.getElementById("light4").style.backgroundColor = "yellow";
 }
-function lightOnYellow4(){
-  document.getElementById('light5').style.backgroundColor='yellow';
+function lightOnYellow4() {
+  document.getElementById("light5").style.backgroundColor = "yellow";
 }
-function lightOnYellow5(){
-  document.getElementById('light6').style.backgroundColor='yellow';
+function lightOnYellow5() {
+  document.getElementById("light6").style.backgroundColor = "yellow";
 }
-function lightOnYellow6(){
-  document.getElementById('light7').style.backgroundColor='yellow';
+function lightOnYellow6() {
+  document.getElementById("light7").style.backgroundColor = "yellow";
 }
-function lightOnYellow7(){
-  document.getElementById('light8').style.backgroundColor='yellow';
+function lightOnYellow7() {
+  document.getElementById("light8").style.backgroundColor = "yellow";
 }
 
-function turnon(){
-  if(light){
-    light=false;
-    document.getElementById('light1').style.backgroundColor='transparent';
-    setTimeout(lightOnTransparent1,200);
-    setTimeout(lightOnTransparent2,600);
-    setTimeout(lightOnTransparent3,800);
-    setTimeout(lightOnTransparent4,1000);
-    setTimeout(lightOnTransparent5,1200);
-    setTimeout(lightOnTransparent6,1400);
-    setTimeout(lightOnTransparent7,1600);
-
-
-  }else{
-    light=true;
-    document.getElementById('light1').style.backgroundColor='yellow';
-    setTimeout(lightOnYellow1,200);
-    setTimeout(lightOnYellow2,600);
-    setTimeout(lightOnYellow3,800);
-    setTimeout(lightOnYellow4,1000);
-    setTimeout(lightOnYellow5,1200);
-    setTimeout(lightOnYellow6,1400);
-    setTimeout(lightOnYellow7,1600);
-    
+function turnon() {
+  if (light) {
+    light = false;
+    document.getElementById("light1").style.backgroundColor = "transparent";
+    setTimeout(lightOnTransparent1, 200);
+    setTimeout(lightOnTransparent2, 600);
+    setTimeout(lightOnTransparent3, 800);
+    setTimeout(lightOnTransparent4, 1000);
+    setTimeout(lightOnTransparent5, 1200);
+    setTimeout(lightOnTransparent6, 1400);
+    setTimeout(lightOnTransparent7, 1600);
+  } else {
+    light = true;
+    document.getElementById("light1").style.backgroundColor = "yellow";
+    setTimeout(lightOnYellow1, 200);
+    setTimeout(lightOnYellow2, 600);
+    setTimeout(lightOnYellow3, 800);
+    setTimeout(lightOnYellow4, 1000);
+    setTimeout(lightOnYellow5, 1200);
+    setTimeout(lightOnYellow6, 1400);
+    setTimeout(lightOnYellow7, 1600);
   }
 }
 
-function Work(){
-    return (
-      <div id="Work"  >
-        <div id='part1'>
-          <div id='workContainer'>
-              <a href='https://smartbrain-reg.herokuapp.com/'>
-                <div id='workCard1'>
-                  <h2>Face Regconigtion Website</h2>
-                  <h3>Heroku App</h3>
-                  <br id='br1'></br>
-                  <div id='pjImgfixer1'>
-                        <div id='pjImgContainer1'>
-                          <img src={pj4} alt=''></img>
-                        </div>
-                  </div>
+function Work() {
+  return (
+    <div id="Work">
+      <div id="part1">
+        <div id="workContainer">
+          <a href="https://cp.certmetrics.com/amazon/en/public/verify/credential/adbba4828a2d4d1e8f8a12daff364dd0">
+            <div id="workCard1">
+              <h2>AWS CERTIFIED DEVELOPER - ASSOCIATE</h2>
+              <h3>AWS Services</h3>
+              <br id="br1"></br>
+              <div id="pjImgfixer1">
+                <div id="pjImgContainer1">
+                  <img src={pj4} alt=""></img>
                 </div>
-              </a>
-              <a href='https://api.practera.com/portfolio/1628/nguye3hg%40mail.uc.edu'>
-                <div id='workCard2'>
-                  <h2>WACE Global Student Collaborative Project</h2>
-                  <h3>Sportandi</h3> 
-                  <div id='pjImgfixer2'>
-                        <div id='pjImgContainer2'>
-                          <img src={pj2} alt=''></img>
-
-                        </div>
-                  </div>
+              </div>
+            </div>
+          </a>
+          <a href="https://d7u7nkp3zjlik.cloudfront.net/About">
+            <div id="workCard2">
+              <h2>IRAS LAB WEBSITE</h2>
+              <h3>IRAS LAB</h3>
+              <div id="pjImgfixer2">
+                <div id="pjImgContainer2">
+                  <img src={pj2} alt=""></img>
                 </div>
-              </a>
-              <a href='https://makeuc-pygame.netlify.app/'>
-                <div id='workCard3'>
-                  <h2>WarGame</h2>
-                  <h3>MakeUC Hackathon</h3> 
-                  <br id='br3'></br>
-                  <div id='pjImgfixer3'>
-                        <div id='pjImgContainer3'>
-                          <img src={pj3} alt=''></img>
-                        </div>
-                  </div>
+              </div>
+            </div>
+          </a>
+          <a href="https://github.com/nguye3hg/finace-tracker">
+            <div id="workCard3">
+              <h2>Finance Tracker App</h2>
+              <h3>Personal Project</h3>
+              <br id="br3"></br>
+              <div id="pjImgfixer3">
+                <div id="pjImgContainer3">
+                  <img src={pj3} alt=""></img>
                 </div>
-              </a>
-              <a href='https://nguye3hg.netlify.app/'>
-                <div id='workCard4'>
-                  <h2>Old Portfolio Website</h2>
-                  <h3>Netlify App</h3>
-                  <br id='br4' ></br>
-                  <div id='pjImgfixer4'>
-                        <div id='pjImgContainer4'>
-                          <img src={pj1} alt=''></img>
-                        </div>
-                  </div>
-                    
+              </div>
+            </div>
+          </a>
+          <a href="https://makeuc.netlify.app/">
+            <div id="workCard4">
+              <h2>The Crox</h2>
+              <h3>MakeUC Hackathon</h3>
+              <br id="br4"></br>
+              <div id="pjImgfixer4">
+                <div id="pjImgContainer4">
+                  <img src={pj1} alt=""></img>
                 </div>
-              </a>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div id="lampContainer">
+          <div id="lamp">
+            <div className="rope" id="rope1"></div>
+            <div className="cone" id="cone1"></div>
+            <div className="bulb" id="bulb1"></div>
+            <div className="light" id="light1"></div>
           </div>
-          <div id='lampContainer'>
-            <div id="lamp" >
-              <div className='rope' id='rope1'></div>
-              <div className='cone' id='cone1'></div>
-              <div className='bulb' id='bulb1'></div>
-              <div className='light' id='light1'></div>
-            </div>
-            <div id="lamp1" >
-              <div className='rope' id='rope2'></div>
-              <div className='cone' id='cone1'></div>
-              <div className='bulb' id='bulb1'></div>
-              <div className='light' id='light2'></div>
-            </div>
-            <div id="lamp2" >
-              <div className='rope' id='rope3'></div>
-              <div className='cone' id='cone1'></div>
-              <div className='bulb' id='bulb1'></div>
-              <div className='light' id='light3'></div>
-            </div>
-            <div id="lamp3" >
-              <div className='rope' id='rope4'></div>
-              <div className='cone' id='cone1'></div>
-              <div className='bulb' id='bulb1'></div>
-              <div className='light' id='light4'></div>
-            </div>
-            <div id="lamp4" >
-              <div className='rope' id='rope5'></div>
-              <div className='cone' id='cone1'></div>
-              <div className='bulb' id='bulb1'></div>
-              <div className='light' id='light5'></div>
-            </div>
-            <div id="lamp5" >
-              <div className='rope' id='rope6'></div>
-              <div className='cone' id='cone1'></div>
-              <div className='bulb' id='bulb1'></div>
-              <div className='light' id='light6'></div>
-            </div>
-            <div id="lamp6" >
-              <div className='rope' id='rope7'></div>
-              <div className='cone' id='cone1'></div>
-              <div className='bulb' id='bulb1'></div>
-              <div className='light' id='light7'></div>
-            </div>
-            <div id="lamp7" >
-              <div className='rope' id='rope8'></div>
-              <div className='cone' id='cone1'></div>
-              <div className='bulb' id='bulb1'></div>
-              <div className='light' id='light8'></div>
-            </div>
+          <div id="lamp1">
+            <div className="rope" id="rope2"></div>
+            <div className="cone" id="cone1"></div>
+            <div className="bulb" id="bulb1"></div>
+            <div className="light" id="light2"></div>
+          </div>
+          <div id="lamp2">
+            <div className="rope" id="rope3"></div>
+            <div className="cone" id="cone1"></div>
+            <div className="bulb" id="bulb1"></div>
+            <div className="light" id="light3"></div>
+          </div>
+          <div id="lamp3">
+            <div className="rope" id="rope4"></div>
+            <div className="cone" id="cone1"></div>
+            <div className="bulb" id="bulb1"></div>
+            <div className="light" id="light4"></div>
+          </div>
+          <div id="lamp4">
+            <div className="rope" id="rope5"></div>
+            <div className="cone" id="cone1"></div>
+            <div className="bulb" id="bulb1"></div>
+            <div className="light" id="light5"></div>
+          </div>
+          <div id="lamp5">
+            <div className="rope" id="rope6"></div>
+            <div className="cone" id="cone1"></div>
+            <div className="bulb" id="bulb1"></div>
+            <div className="light" id="light6"></div>
+          </div>
+          <div id="lamp6">
+            <div className="rope" id="rope7"></div>
+            <div className="cone" id="cone1"></div>
+            <div className="bulb" id="bulb1"></div>
+            <div className="light" id="light7"></div>
+          </div>
+          <div id="lamp7">
+            <div className="rope" id="rope8"></div>
+            <div className="cone" id="cone1"></div>
+            <div className="bulb" id="bulb1"></div>
+            <div className="light" id="light8"></div>
           </div>
         </div>
-        <div id='transition'></div>
-        <div id='part2' onClick={turnon}>
-          < Contact />
-          
-          <div id='filler'></div> 
-        </div> 
-        
-           
       </div>
-    );
+      <div id="transition"></div>
+      <div id="part2" onClick={turnon}>
+        <Contact />
+
+        <div id="filler"></div>
+      </div>
+    </div>
+  );
 }
-  
-  export default Work;
+
+export default Work;
